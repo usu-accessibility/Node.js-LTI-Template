bccab972b4231d0f463a2e156431749b236a3278
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-11-28 12:26:08 -0700

    fixed few bugs

--- src/services/elasti-cache-service.js
+++ src/services/elasti-cache-service.js
@@ -1,57 +1,7 @@
-// const redis = require('redis');
+import { createClient } from 'redis';
 
-// // Replace these values with your ElastiCache endpoint and port
-// const redisClient = redis.createClient({
-//     rootNodes: [
-//         {
-//             url: 'redis://accessibilitycache.fif8xk.ng.0001.usw2.cache.amazonaws.com:6379'
-//         }
-//     ]});
+const client = createClient();
 
-// console.log("connecting to elasticache");
+client.on('error', err => console.log('Redis Client Error', err));
 
-// redisClient.on('connect', () => {
-//     console.log('Connected to Redis');
-// });
-
-// redisClient.on('error', (err) => {
-//     console.error(`Redis Error: ${err}`);
-// });
-
-// redisClient.set('my_key', 'my_value', (err, response) => {
-//     if (err) {
-//       console.error(`Set error: ${err}`);
-//     } else {
-//       console.log(`Set response: ${response}`);
-//     }
-//   });
-
-var RedisClustr = require('redis-clustr');
-var RedisClient = require('redis');
-
-var redis = new RedisClustr({
-    servers: [
-        {
-            host: "accessibilitycache.fif8xk.ng.0001.usw2.cache.amazonaws.com:6379",
-            port: 6379
-        }
-    ],
-    createClient: function (port, host) {
-        // this is the default behaviour
-        return RedisClient.createClient(port, host);
-    }
-});
-
-//connect to redis
-redis.on("connect", function () {
-  console.log("connected to redis");
-});
-
-// //check the functioning
-// redis.set("framework", "No", function (err, reply) {
-//   console.log("redis.set " , reply);
-// });
-
-// redis.get("framework", function (err, reply) {
-//   console.log("redis.get ", reply);
-// });
+await client.connect();
