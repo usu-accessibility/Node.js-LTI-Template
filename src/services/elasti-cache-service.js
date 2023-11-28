e6cca8b8c61de04bfa1d144d4414bd89c01e1b59
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-11-28 12:26:23 -0700

    fixed few bugs

--- src/services/elasti-cache-service.js
+++ src/services/elasti-cache-service.js
@@ -5,3 +5,13 @@ const client = createClient();
 client.on('error', err => console.log('Redis Client Error', err));
 
 await client.connect();
+
+await client.hSet('user-session:123', {
+    name: 'John',
+    surname: 'Smith',
+    company: 'Redis',
+    age: 29
+})
+
+let userSession = await client.hGetAll('user-session:123');
+console.log(JSON.stringify(userSession, null, 2));
